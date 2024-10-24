5aae819f122e0db843fa86e8179317a2f48a3fa3
970dishu <970scr1@gmail.com>
2024-10-24 23:59:54 +0530

    bg added

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ document.getElementById('togglerBtn').addEventListener('click', function () {
     const navLinks = document.getElementById('navLinks');
     navLinks.classList.toggle('active');
 });
+function mouseover() {
+    document.getElementById("get").style.color = "white";
+    document.getElementById("get").style.backgroundColor = "rgb(30, 30, 30)";
+    
+}
+
+function mouseout() {
+    document.getElementById("get").style.color = "white";
+    document.getElementById("get").style.backgroundColor = "black";
+}
